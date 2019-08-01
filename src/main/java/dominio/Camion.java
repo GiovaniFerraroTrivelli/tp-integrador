69d397f20037741ab7f928853613b2bd167b6c86
Bode Matias <32490307+BodeMatias@users.noreply.github.com>
2019-08-01 20:27:10 -0300

    Update Camion.java

--- src/main/java/dominio/Camion.java
+++ src/main/java/dominio/Camion.java
@@ -76,34 +76,33 @@ public class Camion {
 		this.capacidad = capacidad;
 	}
 	
-	public boolean[] resolver(int[] peso, int[] valor) {  // valor = costo insumo
-		int N = peso.length; // items
-		int W = this.capacidad + 1; // max peso
+	public boolean[] resolver(Float[] peso, Float[] valor) {  // valor = costo insumo
+		Integer N = peso.length; // items
+		Integer W = this.capacidad + 1; // max peso
 		  
-		int[][] opt = new int[N][W]; //matriz que guarda el valor de cada escenario
+		Float[][] opt = new Float[N][W]; //matriz que guarda el valor de cada escenario
 		boolean[][] sol = new boolean[N][W]; // matriz que guarda si el elemento esta en el escenario
 		    
 		for (int n = 0; n < N; n++) {
 			for (int w = 0; w < W; w++) {
-				int option1 = n < 1 ? 0 : opt[n -1][w]; //completar
-		        int option2 = Integer.MIN_VALUE;
+				Float option1 = n < 1 ? 0 : opt[n -1][w]; //completar
+				Float option2 = Float.MIN_VALUE;
 		        if (peso[n] <= w) { //Hay espacio en la mochila?
 		        	if(n != 0)
-		        		option2 = valor[n] + opt[n-1][w-peso[n]]; //actualizar el valor de agregar a la mochila el elemento
+		        		option2 = valor[n] + opt[n-1][(w-(Math.round(peso[n])))]; //actualizar el valor de agregar a la mochila el elemento
 		         	else
-		            	option2 = valor[n] + opt[n][w-peso[n]];
+		            	option2 = valor[n] + opt[n][w-(Math.round(peso[n]))];
 		         }
 		                
 		            opt[n][w] = Math.max(option1, option2);
 		            sol[n][w] = (option2 > option1);
 		     }
 		}
-
 		boolean[] esSolucion= new boolean[N];
-		for (int n = N-1, w = W-1; n >= 0; n--) {
+		for (int n = N-1, w = N-1; n >= 0; n--) {
 			if (sol[n][w]) {
 		    	esSolucion [n] = true;
-		    	w = w - peso[n];
+		    	w = w - (Math.round(peso[n]));
 		    } else {
 		            esSolucion [n] = false;
 		    }
